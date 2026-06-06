2eb7c68e9ad3aa9be2bcab1ea7cfe9427bebbf98
Tanzia Rahman <tanziarahman566@gmail.com>
2026-06-06 09:52:03 +0600

    updated server.js and generateToke.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
+import { errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 connectDB();
@@ -10,6 +12,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+// Error Handler (must be last)
+app.use(errorHandler);
 
 app.get('/', (req, res) => {
   res.send('🐝 CollabHive API is running...');
