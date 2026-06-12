df8b12211e8264c8866ae11672780ca2b0e3b6c1
Tanzia Rahman <tanziarahman566@gmail.com>
2026-06-13 01:45:30 +0600

    dummy dshbaord created

---
+++ client/src/pages/Dashboard/Dashbiard.jsx
@@ -0,0 +1,11 @@
+// Dashboard.jsx
+import Navbar from "../../components/Navbar/Navbar";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <Navbar />
+      {/* Rest of your dashboard content */}
+    </div>
+  );
+}
