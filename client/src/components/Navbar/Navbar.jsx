65ec8db2c8bad76543177bfc614f76f5e360fec5
Tanzia Rahman <tanziarahman566@gmail.com>
2026-07-13 10:08:57 +0600

    posts button added in and create project button updated

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -59,11 +59,12 @@ export default function Navbar() {
       <div className="navbar-right">
         {/* Create Project Button */}
         <button className="create-project-btn" onClick={() => navigate("/create-project")}>
-          <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-            <line x1="12" y1="5" x2="12" y2="19" />
-            <line x1="5" y1="12" x2="19" y2="12" />
-          </svg>
           Create Project
+        </button>
+
+        {/* My Posts Button */}
+        <button className="my-posts-btn" onClick={() => navigate("/my-posts")}>
+          Posts
         </button>
 
         {/* Notifications Bell */}
